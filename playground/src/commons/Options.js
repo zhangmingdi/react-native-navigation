79e2df534ecf03f8365ad6aca6cc0aa2a9e6e23a
Guy Carmeli <guyc@wix.com>
2019-06-22 12:47:20 +0300

    control certain elements with debug flags
    Some elements in the playground app are useful for testing but we wouldn't want to have them
    enabled at all times as most of the time they're a distraction.

--- playground/src/commons/Options.js
+++ playground/src/commons/Options.js
@@ -2,8 +2,9 @@ const { Navigation } = require('react-native-navigation');
 const Colors = require('./Colors');
 const { Dimensions, PixelRatio } = require('react-native');
 const height = PixelRatio.getPixelSizeForLayoutSize(Dimensions.get('window').height) * 0.7;
-const SHOW_DURATION = 230 * 1;
+const { useSlowOpenScreenAnimations } = require('../flags');
 
+const SHOW_DURATION = 230 * 8;
 
 const setDefaultOptions = () => Navigation.setDefaultOptions({
   layout: {
@@ -17,39 +18,43 @@ const setDefaultOptions = () => Navigation.setDefaultOptions({
     selectedIconColor: Colors.primary,
     selectedTextColor: Colors.primary
   },
-  _animations: {
-    showModal: {
-      waitForRender: true,
+  animations: {
+    ...useSlowOpenScreenAnimations ? slowOpenScreenAnimations : {}   
+  }
+});
+
+const slowOpenScreenAnimations = {
+  showModal: {
+    waitForRender: true,
+    y: {
+      from: height,
+      to: 0,
+      duration: SHOW_DURATION,
+      interpolation: 'accelerateDecelerate'
+    },
+    alpha: {
+      from: 0.7,
+      to: 1,
+      duration: SHOW_DURATION,
+      interpolation: 'accelerate'
+    }
+  },
+  push: {
+    waitForRender: true,
+    content: {
+      alpha: {
+        from: 0.6,
+        to: 1,
+        duration: SHOW_DURATION,
+      },
       y: {
         from: height,
         to: 0,
         duration: SHOW_DURATION,
-        interpolation: 'accelerateDecelerate'
-      },
-      alpha: {
-        from: 0.7,
-        to: 1,
-        duration: SHOW_DURATION,
-        interpolation: 'accelerate'
-      }
-    },
-    push: {
-      waitForRender: true,
-      content: {
-        alpha: {
-          from: 0.6,
-          to: 1,
-          duration: SHOW_DURATION,
-        },
-        y: {
-          from: height,
-          to: 0,
-          duration: SHOW_DURATION,
-        }
       }
     }
   }
-});
+}
 
 module.exports = {
   setDefaultOptions
