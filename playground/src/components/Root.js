3d04ba2d03a5d4df59099b23ebce7ba4b6da9e1a
Guy Carmeli <guyc@wix.com>
2019-06-22 12:47:20 +0300

    control certain elements with debug flags
    Some elements in the playground app are useful for testing but we wouldn't want to have them
    enabled at all times as most of the time they're a distraction.

--- playground/src/components/Root.js
+++ playground/src/components/Root.js
@@ -1,16 +1,23 @@
 const React = require('react');
-const {TextInput} = require('react-native');
+const { TextInput } = require('react-native');
 const { View, Text, StyleSheet, ScrollView } = require('react-native');
+const { showTextInputToTestKeyboardInteraction } = require('../flags');
 
 module.exports = (props) =>
   <ScrollView contentContainerStyle={[styles.scrollView, props.style]}>
     {props.children}
-    {props.componentId && <View style={styles.footer}>
-      <TextInput placeholder='Input' style={{borderWidth: 1, borderRadius: 10, height: 40, width: 100}}/>
-      {props.footer && <Text style={styles.footerText}>{props.footer}</Text>}
-      <Text style={styles.footerText}>{`this.props.componentId = ${props.componentId}`}</Text>
-    </View>}
+    {renderFooter(props)}
   </ScrollView>;
+
+const renderFooter = (props) => props.componentId && <View style={styles.footer}>
+  {renderInput()}
+  {renderFooterText(props)}
+  {renderComponentId(props)}
+</View>;
+
+const renderInput = () =>  showTextInputToTestKeyboardInteraction && <TextInput placeholder='Input' style={{ borderWidth: 1, borderRadius: 10, height: 40, width: 100 }} />;
+const renderFooterText = (props) => { props.footer && <Text style={styles.footerText}>{props.footer}</Text> };
+const renderComponentId = (props) => <Text style={styles.footerText}>{`this.props.componentId = ${props.componentId}`}</Text>;
 
 const styles = StyleSheet.create({
   scrollView: {
