e1b93643e6fd978d1d65be3318c628c5af4bb779
Guy Carmeli <guyc@wix.com>
2019-07-01 11:38:30 +0300

    Add react-native-keyboard-tracking-view

--- playground/src/components/Root.js
+++ playground/src/components/Root.js
@@ -1,13 +1,17 @@
 const React = require('react');
 const { TextInput } = require('react-native');
-const { View, Text, StyleSheet, ScrollView } = require('react-native');
+const { View, Text, StyleSheet, ScrollView,  } = require('react-native');
+const { KeyboardAwareInsetsView } = require('react-native-keyboard-tracking-view');
 const { showTextInputToTestKeyboardInteraction } = require('../flags');
 
 module.exports = (props) =>
-  <ScrollView contentContainerStyle={[styles.scrollView, props.style]}>
-    {props.children}
-    {renderFooter(props)}
-  </ScrollView>;
+  <View style={styles.root}>
+    <ScrollView contentContainerStyle={[styles.scrollView, props.style]}>
+      {props.children}
+      {renderFooter(props)}
+    </ScrollView>
+    <KeyboardAwareInsetsView />
+  </View>
 
 const renderFooter = (props) => props.componentId && <View style={styles.footer}>
   {renderInput()}
@@ -15,11 +19,14 @@ const renderFooter = (props) => props.componentId && <View style={styles.footer}
   {renderComponentId(props)}
 </View>;
 
-const renderInput = () =>  showTextInputToTestKeyboardInteraction && <TextInput placeholder='Input' style={{ borderWidth: 1, borderRadius: 10, height: 40, width: 100 }} />;
+const renderInput = () => showTextInputToTestKeyboardInteraction && <TextInput placeholder='Input' style={{ borderWidth: 1, borderRadius: 10, height: 40, width: 100 }} />;
 const renderFooterText = (props) => { props.footer && <Text style={styles.footerText}>{props.footer}</Text> };
 const renderComponentId = (props) => <Text style={styles.footerText}>{`this.props.componentId = ${props.componentId}`}</Text>;
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1
+  },
   scrollView: {
     flexGrow: 1,
     alignItems: 'center',
