4b5bde278f8a52d4a13752aaa8f75316ef065f85
Guy Carmeli <guyc@wix.com>
2020-01-17 17:19:29 +0200

    Refactor
    Move reparent logic to TransitionAnimatorCreator as it's logic is too specific

--- lib/android/app/src/main/java/com/reactnativenavigation/views/element/TransitionAnimatorCreator.kt
+++ lib/android/app/src/main/java/com/reactnativenavigation/views/element/TransitionAnimatorCreator.kt
@@ -4,9 +4,12 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
 import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import com.reactnativenavigation.R
 import com.reactnativenavigation.parse.AnimationOptions
 import com.reactnativenavigation.utils.CollectionUtils
+import com.reactnativenavigation.utils.ViewTags
 import com.reactnativenavigation.utils.ViewUtils
 import com.reactnativenavigation.viewcontrollers.ViewController
 import java.util.*
@@ -46,7 +49,8 @@ open class TransitionAnimatorCreator {
                 allTransitions.sortBy { it.view.getTag(R.id.original_index_in_parent) as Int}
                 allTransitions.forEach {
                     it.viewController.requireParentController().removeOverlay(it.view)
-                    ViewUtils.returnToOriginalParent(it.view)
+                    it.viewController.removeOverlay(it.view)
+                    returnToOriginalParent(it.view)
                 }
                 CollectionUtils.forEach(transitions.validSharedElementTransitions) { transition: SharedElementTransition ->
 //                    toScreen.removeOverlay(transition.to)
@@ -72,7 +76,7 @@ open class TransitionAnimatorCreator {
     }
 
     private fun createSharedElementAnimator(toScreen: ViewController<*>, transition: SharedElementTransition): AnimatorSet {
-        ViewUtils.reparent(transition.to) { child: View? -> toScreen.requireParentController().addOverlay(child) }
+        reparent(toScreen.requireParentController(), transition.to, 0)
         val set = AnimatorSet()
         set.playTogether(transition.createAnimators())
         set.addListener(object : AnimatorListenerAdapter() {
@@ -86,9 +90,41 @@ open class TransitionAnimatorCreator {
     private fun createElementTransitionAnimators(transitions: List<ElementTransition>): List<AnimatorSet> {
         val animators: MutableList<AnimatorSet> = ArrayList()
         for (transition in transitions) {
-            ViewUtils.reparent(transition.view) { view: View? -> transition.viewController.addOverlay(view) }
+            reparent(transition.viewController.requireParentController(), transition.view, transition.viewController.topInset)
             animators.add(transition.createAnimators())
         }
         return animators
+    }
+
+    open fun reparent(vc: ViewController<*>, child: View, topInset: Int) {
+        val biologicalParent = child.parent as ViewGroup
+        child.setTag(R.id.original_parent, biologicalParent)
+        child.setTag(R.id.original_layout_params, child.layoutParams)
+        child.setTag(R.id.original_top, child.top)
+        child.setTag(R.id.original_bottom, child.bottom)
+        child.setTag(R.id.original_right, child.right)
+        child.setTag(R.id.original_left, child.left)
+
+        val loc = ViewUtils.getLocationOnScreen(child)
+        biologicalParent.removeView(child)
+
+        val lp = FrameLayout.LayoutParams(child.layoutParams)
+        lp.topMargin = loc.y + 0
+        lp.leftMargin = loc.x
+        lp.width = child.width
+        lp.height = child.height
+        child.layoutParams = lp
+        vc.addOverlay(child)
+    }
+
+    open fun returnToOriginalParent(view: View) {
+        val parent = ViewTags.get<ViewGroup>(view, R.id.original_parent)
+        val lp = ViewTags.get<ViewGroup.LayoutParams>(view, R.id.original_layout_params)
+        ViewUtils.removeFromParent(view)
+        view.top = ViewTags.get(view, R.id.original_top)
+        view.bottom = ViewTags.get(view, R.id.original_bottom)
+        view.right = ViewTags.get(view, R.id.original_right)
+        view.left = ViewTags.get(view, R.id.original_left)
+        parent.addView(view, lp)
     }
 }
