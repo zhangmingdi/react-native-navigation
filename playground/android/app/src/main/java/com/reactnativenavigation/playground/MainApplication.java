2b50c998bd36e609838aaa8b53905e4c8b2f0c02
Guy Carmeli <guyc@wix.com>
2020-01-23 10:19:29 +0200

    Some progress with autolink support

--- playground/android/app/src/main/java/com/reactnativenavigation/playground/MainApplication.java
+++ playground/android/app/src/main/java/com/reactnativenavigation/playground/MainApplication.java
@@ -1,23 +1,20 @@
 package com.reactnativenavigation.playground;
 
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-
-import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationPackage;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-import androidx.annotation.Nullable;
 
 public class MainApplication extends NavigationApplication {
 
     private final ReactNativeHost mReactNativeHost =
-            new ReactNativeHost(this) {
+            new NavigationReactNativeHost(this) {
                 @Override
                 protected String getJSMainModuleName() {
                     return "index";
@@ -30,13 +27,16 @@ public class MainApplication extends NavigationApplication {
 
                 @Override
                 public List<ReactPackage> getPackages() {
-                    return Arrays.asList(new MainReactPackage(), new NavigationPackage(mReactNativeHost));
+                    ArrayList<ReactPackage> packages = new PackageList(this).getPackages();
+                    packages.add(new NavigationPackage(mReactNativeHost));
+                    return packages;
                 }
             };
 
 
     @Override
     public void onCreate() {
+        SoLoader.init(this, false);
         super.onCreate();
         registerExternalComponent("RNNCustomComponent", new FragmentCreator());
     }
