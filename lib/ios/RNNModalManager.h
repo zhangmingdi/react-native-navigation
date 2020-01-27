0cf67252f387e10d941c155faec1d86ccc8c46a9
yogevbd <yogev132@gmail.com>
2020-01-27 18:53:13 +0200

    Fix dismissModal custom animation

--- lib/ios/RNNModalManager.h
+++ lib/ios/RNNModalManager.h
@@ -1,7 +1,6 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <React/RCTUIManager.h>
-#import "ModalTransitionDelegate.h"
 
 typedef void (^RNNTransitionCompletionBlock)(void);
 typedef void (^RNNTransitionWithComponentIdCompletionBlock)(NSString *componentId);
@@ -20,7 +19,7 @@ typedef void (^RNNTransitionRejectionBlock)(NSString *code, NSString *message, N
 - (instancetype)initWithUIManager:(RCTUIManager *)uiManager;
 
 @property (nonatomic, weak) id<RNNModalManagerDelegate> delegate;
-@property (nonatomic, strong) ModalTransitionDelegate* modalTransitionDelegate;
+
 - (void)showModal:(UIViewController *)viewController animated:(BOOL)animated completion:(RNNTransitionWithComponentIdCompletionBlock)completion;
 - (void)dismissModal:(UIViewController *)viewController completion:(RNNTransitionCompletionBlock)completion;
 - (void)dismissAllModalsAnimated:(BOOL)animated completion:(void (^ __nullable)(void))completion;
