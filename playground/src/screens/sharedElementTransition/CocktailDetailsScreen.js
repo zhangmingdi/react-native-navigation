865b9f4aaa31b81679d1a0ad735f2d3743c6e023
yogevbd <yogev132@gmail.com>
2020-02-03 11:29:41 +0200

    Update CocktailDetailsScreen.js

--- playground/src/screens/sharedElementTransition/CocktailDetailsScreen.js
+++ playground/src/screens/sharedElementTransition/CocktailDetailsScreen.js
@@ -1,5 +1,5 @@
 const React = require('react');
-const { Image, Platform, SafeAreaView, StyleSheet, Text, View } = require('react-native');
+const {Image, Platform, SafeAreaView, StyleSheet, Text, View} = require('react-native');
 
 class CocktailDetailsScreen extends React.Component {
   static options() {
@@ -24,15 +24,18 @@ class CocktailDetailsScreen extends React.Component {
     return (
       <SafeAreaView>
         <View>
-          <View nativeID={'backdrop'} style={[styles.header, { backgroundColor: this.props.color }]}>
-            <Text style={styles.title} nativeID={`title${this.props.id}Dest`}>{this.props.name}</Text>
+          <View nativeID={'backdrop'} style={[styles.header, {backgroundColor: this.props.color}]}>
+            
           </View>
         </View>
-        <Image
-          source={this.props.image}
-          nativeID={`image${this.props.id}Dest`}
-          style={styles.image}
-        />
+        <View style={styles.headerContainer}>
+          <Image
+            source={this.props.image}
+            nativeID={`image${this.props.id}Dest`}
+            style={styles.image}
+          />
+          <Text style={styles.title} nativeID={`title${this.props.id}Dest`}>{this.props.name}</Text>
+        </View>
         <Text
           nativeID='description'
           style={styles.description}>
@@ -57,8 +60,8 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 32,
     color: 'whitesmoke',
-    marginLeft: 24,
-    marginBottom: 16,
+    marginLeft: 15,
+    marginTop: 45,
     zIndex: 2
   },
   description: {
@@ -68,12 +71,15 @@ const styles = StyleSheet.create({
     marginTop: 32,
     marginHorizontal: 24
   },
+  headerContainer: {
+    position: 'absolute',
+    left: 24,
+    top: IMAGE_OFFSET,
+    flexDirection: 'row'
+  },
   image: {
     height: SIZE,
     width: SIZE,
-    position: 'absolute',
-    right: 24,
-    top: IMAGE_OFFSET,
     zIndex: 1
   }
 });
