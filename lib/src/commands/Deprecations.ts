f6973d1166a8c438ab01ef2025130eda1ff7039e
Guy Carmeli <guyc@wix.com>
2020-02-07 09:51:00 +0200

    fix lint warning

--- lib/src/commands/Deprecations.ts
+++ lib/src/commands/Deprecations.ts
@@ -3,17 +3,18 @@ import once from 'lodash/once';
 import { Platform } from 'react-native';
 
 export class Deprecations {
+  private deprecateDrawBehind = once((parentOptions: object) => {
+    this.warnDeprecatedOption('drawBehind', 'Please use SafeAreaView, or ScrollView when appropriate.', parentOptions);
+  });
+
   public onProcessOptions(key: string, parentOptions: Record<string, any>) {
     if (isEqual(key, 'drawBehind') && Platform.OS === 'ios') {
       this.deprecateDrawBehind(parentOptions)
     }
   }
 
-  private deprecateDrawBehind = once((parentOptions: object) => {
-    this.warnDeprecatedOption('drawBehind', 'Please use SafeAreaView, or ScrollView when appropriate.', parentOptions);
-  })
-
   private warnDeprecatedOption(key: string, message: string, parentOptions: object) {
+    // tslint:disable-next-line:no-console
     console.warn(`${key} is deprecated. ${message}`, parentOptions);
   }
 }
