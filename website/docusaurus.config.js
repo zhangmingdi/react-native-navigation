6d66dd17b1c22e5d1d3218a3081e2dde16d7a88b
Guy Carmeli <guyc@wix.com>
2020-02-12 12:14:56 +0200

    Switch to docusaurus2

---
+++ website/docusaurus.config.js
@@ -0,0 +1,91 @@
+module.exports = {
+  title: 'React Native Navigation',
+  tagline: 'The tagline of my site',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'React Native Navigation',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.png',
+      },
+      links: [
+        {to: 'docs/iosManual', label: 'Docs', position: 'left'},
+        {to: 'docs/modal-api', label: 'API', position: 'left'},
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/doc1',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ website/sidebars.js
@@ -0,0 +1,170 @@
+module.exports = {
+  docs: {
+    Installation: [
+      {
+        type: 'category',
+        label: 'iOS',
+        items: [
+          'iosManual',
+          'iosCocoaPods'
+        ]
+      },
+      {
+        'type': 'category',
+        'label': 'Android',
+        'items': [
+          'installingAndroid'
+        ]
+      }
+    ],
+    'Getting Started': [
+      'displayTheAppRoot',
+      'showcases'
+    ],
+    Layouts: [
+      {
+        'type': 'category',
+        'label': 'Stack',
+        'items': [
+          'stack-api',
+          'stack-examples',
+          'stack-options',
+          'stack-buttons'
+        ]
+      },
+      {
+        'type': 'category',
+        'label': 'BottomTabs',
+        'items': [
+          'bottomTabs-selecting',
+          'bottomTabs-visibility',
+          'bottomTabs-dynamic-options',
+          'bottomTabs-options'
+        ]
+      },
+      {
+        'type': 'category',
+        'label': 'SideMenu',
+        'items': [
+          'sideMenu-openProgrammatically',
+          'sideMenu-disable'
+        ]
+      },
+      {
+        'type': 'category',
+        'label': 'ExternalComponent',
+        'items': [
+          'externalComponent-viewGroup',
+          'externalComponent-fragment',
+          'externalComponent-viewController'
+        ]
+      }
+    ]
+  },
+  API: {
+    Commands: [
+      'root-api',
+      'stack-api',
+      'modal-api'
+    ],
+    // Layouts: [
+    //   'layout',
+    //   'container-layout',
+    //   'component',
+    //   'stack',
+    //   'bottomTabs',
+    //   'sideMenu'
+    // ],
+    // Options: [
+    //   'options-root',
+    //   'bottomTabs-options',
+    //   'bottomTab-options',
+    //   'stack-options'
+    // ]
+  }
+};
+
+// {
+//   'docs': {
+//     'Installation': [
+//       {
+//         'type': 'subcategory',
+//         'label': 'iOS',
+//         'ids': [
+//           'iosManual',
+//           'iosCocoaPods'
+//         ]
+//       },
+//       {
+//         'type': 'subcategory',
+//         'label': 'Android',
+//         'ids': [
+//           'installingAndroid'
+//         ]
+//       }
+//     ],
+//     'Getting Started': [
+//       'displayTheAppRoot',
+//       'showcases'
+//     ],
+//     'Layouts2': {
+//       'type': 'category',
+//       'label': 'Layouts2',
+//       'items': [
+//         {
+//           'type': 'category',
+//           'label': 'Stack',
+//           'items': [
+//             'stack-api',
+//             'stack-examples',
+//             'stack-options',
+//             'stack-buttons'
+//           ]
+//         }
+//       ]
+//     },
+//     'Layouts': [
+//       {
+//         'type': 'category',
+//         'label': 'Stack',
+//         'items': [
+//           'stack-api',
+//           'stack-examples',
+//           'stack-options',
+//           'stack-buttons'
+//         ]
+//       },
+//       {
+//         'type': 'subcategory',
+//         'label': 'BottomTabs',
+//         'ids': [
+//           'bottomTabs-selecting',
+//           'bottomTabs-visibility',
+//           'bottomTabs-dynamic-options',
+//           'bottomTabs-options'
+//         ]
+//       },
+//       {
+//         'type': 'subcategory',
+//         'label': 'SideMenu',
+//         'ids': [
+//           'sideMenu-openProgrammatically',
+//           'sideMenu-disable'
+//         ]
+//       },
+//       {
+//         'type': 'subcategory',
+//         'label': 'ExternalComponent',
+//         'ids': [
+//           'externalComponent-viewGroup',
+//           'externalComponent-fragment',
+//           'externalComponent-viewController'
+//         ]
+//       }
+//     ],
+//     'Hierarchy': [
+//       'root'
+//     ]
+//   },
+
+// }
