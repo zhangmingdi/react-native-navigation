7446af6bac5794f17d365a088e4727ab8aeb8d41
yogevbd <yogev132@gmail.com>
2020-04-02 17:34:05 +0300

    Fix home page and logo location

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Redirect} from '@docusaurus/router';
 
 function Home() {
-  return <Redirect to="/docs/installing" />;
+  return <Redirect to="/react-native-navigation/docs/before-you-start" />;
 }
 
 export default Home;
